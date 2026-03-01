25c8894a116be60b30235e3ec46c214e82dd86fb
Vamsinandan BS <61873858+vamsinandanBs@users.noreply.github.com>
2026-03-01 23:38:41 +0530

    Initial commit

---
+++ src/main/java/com/vamsi/spring_data_jpa_hibernate/SpringDataJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.vamsi.spring_data_jpa_hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vamsi/spring_data_jpa_hibernate/SpringDataJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vamsi.spring_data_jpa_hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
